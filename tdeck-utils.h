378446b81397aa2e9977fc48050c43c87e7c16ca
Connor Thomson <blumatrikz@gmail.com>
2026-01-04 13:24:55 -0800

    Update tdeck-utils.h

--- tdeck-utils.h
+++ tdeck-utils.h
@@ -1,12 +1,19 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#ifndef TDECK_UTIL_H
+#define TDECK_UTIL_H
+
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void Display_init(void);
-void Display_printf(const char *fmt, ...);
+void TDeck_init(void);
+void TDeck_printf(const char *fmt, ...);
+void TDeck_printf_color(uint16_t color, const char *fmt, ...);
+
+void TDeck_draw_circle(int x, int y, int r, uint16_t color);
+void TDeck_draw_rectangle(int x, int y, int w, int h, uint16_t color, int filled);
+void TDeck_draw_triangle(int x1,int y1,int x2,int y2,int x3,int y3,uint16_t color,int filled);
 
 #ifdef __cplusplus
 }
