567ff709320881a6902c695fc0e9cdcb0e38d864
Connor Thomson <blumatrikz@gmail.com>
2026-01-04 13:58:59 -0800

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,8 +1,31 @@
 #include "tdeck-utils.h"
 
 void setup() {
-    Display_init();
-    Display_printf("hello\n");
+    int sq = 80;
+
+    int sx = TFT_WIDTH - sq;
+    int sy = 0;
+
+    int cx = sx + sq/2;
+    int cy = sy + sq/2;
+    
+    TDeck_init();
+
+    TDeck_printf_color(TFT_RED, "RED\n");
+    TDeck_printf_color(TFT_GREEN, "GREEN\n");
+    TDeck_printf_color(TFT_BLUE, "BLUE\n");
+    TDeck_printf("NORMAL\n");
+
+    TDeck_draw_rectangle(sx, sy, sq, sq, TFT_BLUE);
+    TDeck_draw_circle(cx, cy, 35, TFT_GREEN);
+    TDeck_draw_triangle(cx, cy-25, cx-22, cy+15, cx+22, cy+15, TFT_RED);
+
 }
 
-void loop() {}
+void loop() {
+    char key = TDeck_getchar();
+    if (key != 0) {
+        TDeck_printf("%c", key);
+    }
+    delay(5);
+}
