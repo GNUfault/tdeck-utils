b632e616f9fa1a502f88a0b0003be64ebf373aee
Connor Thomson <blumatrikz@gmail.com>
2026-01-04 15:43:31 -0800

    Update tdeck-utils.h

--- tdeck-utils.h
+++ tdeck-utils.h
@@ -7,9 +7,9 @@
 void TDeck_init();
 void TDeck_printf(const char *fmt, ...);
 void TDeck_printf_color(uint16_t color, const char *fmt, ...);
-void TDeck_draw_circle(int x, int y, int r, uint16_t color);
-void TDeck_draw_rectangle(int x, int y, int w, int h, uint16_t color);
-void TDeck_draw_triangle(int x1,int y1,int x2,int y2,int x3,int y3,uint16_t color);
+void TDeck_draw_circle(int x, int y, int r, uint16_t color, int filled);
+void TDeck_draw_rectangle(int x, int y, int w, int h, uint16_t color, int filled);
+void TDeck_draw_triangle(int x1,int y1,int x2,int y2,int x3,int y3,uint16_t color, int filled);
 char TDeck_getchar();
 void TDeck_keyboard_backlight(uint8_t value);
 void TDeck_font(uint8_t font);
