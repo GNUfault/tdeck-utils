53f7073843dc2846d81b45b3c7ceac818bc59ec4
Connor Thomson <blumatrikz@gmail.com>
2026-01-04 13:42:53 -0800

    Update tdeck-utils.h

--- tdeck-utils.h
+++ tdeck-utils.h
@@ -1,22 +1,12 @@
-#ifndef TDECK_UTIL_H
-#define TDECK_UTIL_H
+#pragma once
+#include <Arduino.h>
 
-#include <stdint.h>
+#define TFT_WIDTH 320
+#define TFT_HEIGHT 240
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void TDeck_init(void);
+void TDeck_init();
 void TDeck_printf(const char *fmt, ...);
 void TDeck_printf_color(uint16_t color, const char *fmt, ...);
-
 void TDeck_draw_circle(int x, int y, int r, uint16_t color);
-void TDeck_draw_rectangle(int x, int y, int w, int h, uint16_t color, int filled);
-void TDeck_draw_triangle(int x1,int y1,int x2,int y2,int x3,int y3,uint16_t color,int filled);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+void TDeck_draw_rectangle(int x, int y, int w, int h, uint16_t color);
+void TDeck_draw_triangle(int x1,int y1,int x2,int y2,int x3,int y3,uint16_t color);
